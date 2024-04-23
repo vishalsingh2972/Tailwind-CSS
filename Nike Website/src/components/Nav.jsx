0ce44ad129fff41b1f1a3cad5c26047df153e204
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-04-23 08:24:30 +0530

    nav bar

--- Nike Website/src/components/Nav.jsx
+++ Nike Website/src/components/Nav.jsx
@@ -1,6 +1,35 @@
+import {headerLogo} from '../assets/images';
+import {hamburger} from '../assets/icons';
+import {navLinks} from '../constants/index.js';
+
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img 
+            src={headerLogo}
+            alt="Logo"
+            width={130}
+            height={29}/>
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a href={item.href} className="font-montserrat leading-normal text-lg text-slate-gray">
+                {item.label}
+              </a>
+            </li> 
+          ))}
+          
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Products</li>
+          <li>Contact Us</li>
+        </ul>
+        {/* <div>hamburger logo</div> */}     
+      </nav>
+    </header>
   )
 }
 
