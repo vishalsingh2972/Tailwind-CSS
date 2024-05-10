4fedb9aab417985af245a79fcae0c556dff2b30d
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-05-11 01:36:23 +0530

    Hero section done with animations

--- Nike Website/src/sections/Hero.jsx
+++ Nike Website/src/sections/Hero.jsx
@@ -37,14 +37,14 @@ const Hero = () => {
         </div>
       </div>
      
-        <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-centre">
-          <img
-            src={bigShoeImage}
-            alt="shoe collection"
-            width={600}
-            height={500}
-            className="object-contain relative z-10"
-          />
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-centre">
+        <img
+          src={bigShoeImage}
+          alt="shoe collection"
+          width={600}
+          height={500}
+          className="object-contain relative z-10"
+        />
       
         <div className="chote_joote flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe) => (
