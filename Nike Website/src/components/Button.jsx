b0d8c56b11696747d19a7f431f330e73a15585ca
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-05-14 02:10:00 +0530

    Subslll Section completed

--- Nike Website/src/components/Button.jsx
+++ Nike Website/src/components/Button.jsx
@@ -1,7 +1,7 @@
-const Button = ({label, iconURL, backgroundColor, borderColor, textColor}) => {
+const Button = ({label, iconURL, backgroundColor, borderColor, textColor, fullWidth}) => {
   return (
     <button className={`flex justify-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
-    ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : " bg-coral-red text-white border-coral-red"}`}
+    ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}` : " bg-coral-red text-white border-coral-red"} ${fullWidth && 'w-full'}`}
     >    
       {label}
 
