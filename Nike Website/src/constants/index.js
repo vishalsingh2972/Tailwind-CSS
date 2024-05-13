aa165ace56847f2af3a81265a556f2282ab5375a
Vishal Singh <vishalsinghpanwar02@gmail.com>
2024-05-13 06:15:58 +0530

    doing

--- Nike Website/src/constants/index.js
+++ Nike Website/src/constants/index.js
@@ -77,14 +77,14 @@ export const services = [
 export const reviews = [
     {
         imgURL: customer1,
-        customerName: 'Morich Brown',
-        rating: 4.5,
+        customerName: 'Shahrukh Khan',
+        rating: 4.7,
         feedback: "The attention to detail and the quality of the product exceeded my expectations. Highly recommended!"
     },
     {
         imgURL: customer2,
-        customerName: 'Lota Mongeskar',
-        rating: 4.5,
+        customerName: 'Deepika Padukone',
+        rating: 4.2,
         feedback: "The product not only met but exceeded my expectations. I'll definitely be a returning customer!"
     }
 ];
